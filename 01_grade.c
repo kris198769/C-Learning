40c02e1ca98bb0b1623583f6494356ae71b57b46
kris198769 <lhdhly0324@gmail.com>
2026-09-08 23:14:11 +0800

    Add files via upload
    Add grade level practice

---
+++ 01_grade.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int grade = 0;
+	scanf("%d", &grade);
+	if (grade >= 90)
+		printf("A");
+	else if (grade >= 80)
+		printf("B");
+	else if (grade >= 70)
+		printf("C");
+	else if (grade >= 60)
+		printf("D");
+	else printf("F");
+	return 0;
+}
